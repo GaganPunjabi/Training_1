dbcd117e00875f73554b2bdeaea69c127e389abc
GaganPunjabi <gaganpunjabi316@gmail.com>
2020-08-13 19:00:07 +0530

    Removed element oggy

--- remover/Main.java
+++ remover/Main.java
@@ -5,14 +5,19 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void removeOggy(ArrayList<String> names) {
-        return;
+        for (int i = 0; i < names.size(); i++)
+            if(names.get(i).startsWith("oggy"))
+            {
+                names.remove(names.get(i));
+                i--;
+            }
     }
     public static boolean oggyIsRemoved(ArrayList<String> names) {
         List<String> oggys = names.stream().filter(it -> it.contains("oggy")).collect(Collectors.toList());
-        return oggys.size() == 0;
+        return oggys.isEmpty();
     }
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("nobita", "bheem", "oggy", "oggy cockroach", "ninja"));
         removeOggy(names);
         if(oggyIsRemoved(names)) {
             System.out.println("Passed!");
